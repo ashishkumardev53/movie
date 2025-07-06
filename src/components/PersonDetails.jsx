1a2f628481c1b249ece63b36bb68ec3600b43667
ashish kumar <ashishkumardev53@gmail.com>
2025-07-06 21:51:03 +0530

    init

--- src/components/PersonDetails.jsx
+++ src/components/PersonDetails.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const PersonDetails = () => {
   return (
-    <div>
-        person
+    <div className="w-screen h-screen flex justify-center items-center overflow-hidden bg-black">
+      <img src="/404.gif" alt="" />
     </div>
-  )
-}
+  );
+};
 
-export default PersonDetails
+export default PersonDetails;
